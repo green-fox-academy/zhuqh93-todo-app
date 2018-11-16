a0b408aa9fb8c80b9d61a31215da4f497952e89f
Jimmy Zhu <jimmy_zhu@epam.com>
2018-11-16 17:36:44 +0800

    1st commit

---
+++ TODO App/TODO App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TODO_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print_Usage_Information();
+        }
+
+        static void Print_Usage_Information()
+        {
+            Console.WriteLine("Command Line Todo application \n" +
+                              "============================= \n" +
+                              "\n" +
+                              "Command line arguments: \n" +
+                              " -l   Lists all the tasks \n" +
+                              " -a   Adds a new task \n" +
+                              " -r   Removes an task \n" +
+                              " -c   Completes an task \n" +
+                              "\n");
+        }
+    }
+}
---
+++ TODO App/TODO App/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace TODO_App
+{
+    class Task
+    {
+        public System.String Name { get; set; }
+        public System.String FilePath { get; set; }
+        private List<string> taskList = new List<string>();
+        
+        public void ListTask()
+        {
+            string tempStore = "";
+            StreamReader reader = new StreamReader(FilePath);
+
+            while (tempStore != null)
+            {
+                tempStore = reader.ReadLine();
+                taskList.Add(tempStore);
+            }
+
+            int index = 1;
+
+            foreach (var task in taskList)
+            {
+                index++;
+                Console.WriteLine("{0} - {1}", index, task);
+            }
+        }
+    }
+}
